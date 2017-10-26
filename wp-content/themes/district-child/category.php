3e3f6246a38b4671b603dae1243b67f3fe2a2ad3
sairaj <sairaj@ajency.in>
2017-10-26 13:17:17 +0530

    category page listing themes added

--- wp-content/themes/district-child/category.php
+++ wp-content/themes/district-child/category.php
@@ -1,16 +1,15 @@
 <?php
 
-/*
-Template Name: Themes Listing
- */
 get_header();
+
+$term = get_queried_object();
 
 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 $query               = new WP_Query(array(
   'post_type' => 'website_theme',
-   'posts_per_page' => 1,
+   'posts_per_page' => -1,
    'paged' => $paged,
-
+   'cat' =>$term->term_id
  ));
 ?>
 
